b87dbd8c170fdec54b73574a9ea05d7c17c99ecc
Estiven Cano <estivencano99@gmail.com>
2023-06-29 18:21:57 -0500

    [Add] Abstract factory content

--- src/components/navigation/RightSideNav.tsx
+++ src/components/navigation/RightSideNav.tsx
@@ -10,7 +10,7 @@ const RightLink = dynamic(() => import("./RightLink"));
 
 export const RightSideNav: FC<Props> = ({ tags }) => {
   return (
-    <aside className='w-full h-full sticky top-20 self-start pl-8 md:flex flex-col gap-4 hidden max-w-xs overflow-hidden overflow-y-auto'>
+    <aside className='w-full h-full sticky top-20 self-start pl-8 lg:flex flex-col gap-4 hidden max-w-xs overflow-hidden overflow-y-auto'>
       <h2 className='font-bold'>On this page</h2>
       <div className='flex flex-col capitalize gap-2'>
         {tags &&
@@ -19,7 +19,7 @@ export const RightSideNav: FC<Props> = ({ tags }) => {
 
             return (
               <span className={`flex items-center pl-${level}`} key={i}>
-                <CircleDashed size={9} />
+                <CircleDashed size={9} className='shrink-0' />
                 <RightLink id={id} />
               </span>
             );
