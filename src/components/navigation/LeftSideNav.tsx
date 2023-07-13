5b1b23a85344035bca879daae2942ae8a705219e
Estiven Cano <estivencano99@gmail.com>
2023-07-13 18:37:18 -0500

    [Add] ScrollToElement async function

--- src/components/navigation/LeftSideNav.tsx
+++ src/components/navigation/LeftSideNav.tsx
@@ -7,7 +7,7 @@ import {
   AccordionTrigger,
   AccordionContent,
 } from "../ui/accordion";
-import { FC, Fragment, useLayoutEffect, useRef } from "react";
+import { FC, Fragment, useEffect, useRef } from "react";
 import React from "react";
 import Link from "next/link";
 
@@ -18,12 +18,17 @@ interface Props {
 export const LeftSideNav: FC<Props> = ({ patternName }) => {
   const ref = useRef<HTMLDivElement>(null);
 
-  useLayoutEffect(() => {
-    const currentPattern = document.getElementById(patternName);
+  useEffect(() => {
+    const goToElement = async () => {
+      const currentPattern = await document.getElementById(patternName);
 
-    ref.current?.scroll({
-      top: currentPattern?.getBoundingClientRect().top || 0 + window.scrollY,
-    });
+      ref.current?.scroll({
+        top:
+          (currentPattern?.getBoundingClientRect().top || 0) + window.scrollY,
+      });
+    };
+
+    goToElement();
   }, [patternName]);
 
   return (
@@ -58,20 +63,6 @@ export const LeftSideNav: FC<Props> = ({ patternName }) => {
           </Fragment>
         ))}
       </Accordion>
-      {/* <dl className='max-w-sm hidden scroll-style md:block pr-2 mr-8 sticky top-20 self-start max-h-[calc(100vh-85px)] overflow-y-auto'>
-        {patterns.map(({ category, list }) => (
-          <Fragment key={category}>
-            <dt className='capitalize bg-destructive/5 dark:bg-red-400/10 text-destructive dark:text-red-400 text-sm rounded-md font-bold p-2'>
-              {category.replaceAll("-", " ")}
-            </dt>
-            {list.map((pattern) => (
-              <dd className='capitalize text-sm my-1 p-1' key={pattern.id}>
-                {pattern.name.replaceAll("-", " ")}
-              </dd>
-            ))}
-          </Fragment>
-        ))}
-      </dl> */}
     </div>
   );
 };
